8a0fb004c44738926b08fb0b04fd2c7e83495474
Jpegx100 <jpegx100@gmail.com>
2014-05-24 22:53:19 -0300

    Calculadora
    Calculadora com interface gráfica em Java.

--- Janela.java
+++ Janela.java
@@ -7,47 +7,111 @@
 package Graphics;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  *
  * @author Patricio JP
  */
 public class Janela extends JFrame{
    private JFrame jan;
-   private JButton bt1, bt2, bt3;
+   private JButton bt1, bt2, bt3, bt4;
    private JLabel lb1, label2, label3, label4;
+   private JTextField tfnum1, tfnum2, tfresult;
+   private float num1, num2, result;
    public Janela(){
+       innitComp();
+       defEvent();
+   }
+   public void defEvent(){
+       bt1.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               num1 = Float.parseFloat(tfnum1.getText());
+               num2 = Float.parseFloat(tfnum2.getText());
+               result = num1 + num2;
+               tfresult.setText(String.valueOf(result));
+           }
+       });
+       bt2.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               num1 = Float.parseFloat(tfnum1.getText());
+               num2 = Float.parseFloat(tfnum2.getText());
+               result = num1 - num2;
+               tfresult.setText(String.valueOf(result));
+           }
+       });
+       bt3.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               num1 = Float.parseFloat(tfnum1.getText());
+               num2 = Float.parseFloat(tfnum2.getText());
+               result = num1 * num2;
+               tfresult.setText(String.valueOf(result));
+           }
+       });
+       bt4.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               num1 = Float.parseFloat(tfnum1.getText());
+               num2 = Float.parseFloat(tfnum2.getText());
+               result = num1 / num2;
+               tfresult.setText(String.valueOf(result));
+           }
+       });
+   }
+   
+   public void innitComp(){
        jan = new JFrame();
-       jan.setSize(300, 300);
-       jan.getContentPane().setBackground(Color.black);
+       jan.setSize(260, 225);
+       jan.getContentPane().setBackground(Color.white);
+       jan.setLocation(300, 300);
        jan.setVisible(true);
-       jan.setTitle("Aplicação Swing");
+       jan.setTitle("XCalc");
        //Criando Labels
-       lb1 = new JLabel("Label 1");
-       lb1.setBounds(40, 20, 50, 50);
-       lb1.setForeground(Color.white);
-       label2 = new JLabel("Label 2");
-       label2.setBounds(40, 50, 50, 50);
-       label2.setForeground(Color.white);
-       label3 = new JLabel("Label 3");
-       label3.setBounds(40, 80, 50, 50);
-       label3.setForeground(Color.white);
+       lb1 = new JLabel("1º Número: ");
+       lb1.setBounds(20, 20, 100, 50);
+       lb1.setForeground(Color.black);
+       label2 = new JLabel("2º Número:");
+       label2.setBounds(20, 50, 100, 50);
+       label2.setForeground(Color.black);
+       label3 = new JLabel("Resultado:");
+       label3.setBounds(20, 125, 100, 50);
+       label3.setForeground(Color.black);
        label4 = new JLabel(".");
        label4.setBounds(40, 110, 50, 50);
-       label4.setForeground(Color.black);
+       label4.setForeground(Color.white);
+       //Criando os TextFields
+       tfnum1 = new JTextField();
+       tfnum1.setBounds(100, 33, 133, 23);
+       tfnum2 = new JTextField();
+       tfnum2.setBounds(100, 63, 133, 23);
+       tfresult = new JTextField();
+       tfresult.setBounds(102, 138, 133, 23);
        
        //Cria Botoes
-       bt1 = new JButton("Botao 01");
-       bt1.setBounds(100, 33, 90, 23);
-       bt2 = new JButton("Botao 02");
-       bt2.setBounds(100, 63, 90, 23);
-       bt3 = new JButton("Botao 03");
-       bt3.setBounds(100, 93, 90, 23);
+       bt1 = new JButton("+");
+       bt1.setBounds(20, 100, 50, 23);
+       bt2 = new JButton("-");
+       bt2.setBounds(75, 100, 50, 23);
+       bt3 = new JButton("*");
+       bt3.setBounds(130, 100, 50, 23);
+       bt4 = new JButton("/");
+       bt4.setBounds(185, 100, 50, 23);
+       
+       //Adcionando elementos ao frame
+       jan.getContentPane().add(tfnum1);
+       jan.getContentPane().add(tfnum2);      
+       jan.getContentPane().add(tfresult);      
        jan.getContentPane().add(bt1);
        jan.getContentPane().add(bt2);
        jan.getContentPane().add(bt3);
+       jan.getContentPane().add(bt4);
        jan.getContentPane().add(lb1);
        jan.getContentPane().add(label2);
        jan.getContentPane().add(label3);
        jan.getContentPane().add(label4);
+       jan.setResizable(false);
    }
 }
